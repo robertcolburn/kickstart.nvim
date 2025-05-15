5de9988b5640c54cd731df6a4a88cf270aa2391d
robertcolburn <60761375+robertcolburn@users.noreply.github.com>
2025-05-15 10:34:47 -0500

    Change toggleterm terminal to powershell

--- init.lua
+++ init.lua
@@ -89,6 +89,19 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+local powershell_options = {
+  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
