1719eb33adf3a1710ba262e7b31d36b5d0deb87e
robertcolburn <60761375+robertcolburn@users.noreply.github.com>
2025-06-26 11:59:34 -0500

    Add Roslyn LSP

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -77,4 +77,13 @@ return {
       vim.o.mousemoveevent = true
     end,
   },
+  {
+    'seblyng/roslyn.nvim',
+    ft = 'cs',
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+      -- your configuration comes here; leave empty for default settings
+    },
+  },
 }
