650275e17bb023065378e86eb067a193fbc70830
robertcolburn <60761375+robertcolburn@users.noreply.github.com>
2025-07-10 16:46:46 -0500

    Add Roslyn LSP

--- init.lua
+++ init.lua
@@ -460,7 +460,15 @@ require('lazy').setup({
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'mason-org/mason.nvim', opts = {} },
+      {
+        'mason-org/mason.nvim',
+        opts = {
+          registries = {
+            'github:mason-org/mason-registry',
+            'github:Crashdummyy/mason-registry',
+          },
+        },
+      },
       'mason-org/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
@@ -652,6 +660,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         basedpyright = {},
+        roslyn = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
