6df1e7088165edafedb8693efcd782b20f3c2bf0
robertcolburn <60761375+robertcolburn@users.noreply.github.com>
2025-07-10 19:20:52 -0500

    Add neotest-dotnet plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -93,4 +93,21 @@ return {
     },
   },
   { 'Issafalcon/neotest-dotnet' },
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'Issafalcon/neotest-dotnet',
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-dotnet',
+        },
+      }
+    end,
+  },
 }
