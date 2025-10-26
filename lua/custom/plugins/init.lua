8dd2c529472185d481285c850be06ee77eb50e47
robertcolburn <60761375+robertcolburn@users.noreply.github.com>
2025-10-26 16:15:36 -0500

    Update deprecated hover.nvim options

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -41,20 +41,20 @@ return {
   {
     'lewis6991/hover.nvim',
     config = function()
-      require('hover').setup {
-        init = function()
-          -- Require providers
-          require 'hover.providers.lsp'
-          -- require('hover.providers.gh')
-          -- require('hover.providers.gh_user')
-          -- require('hover.providers.jira')
-          require 'hover.providers.dap'
-          -- require('hover.providers.fold_preview')
-          require 'hover.providers.diagnostic'
-          -- require('hover.providers.man')
-          -- require('hover.providers.dictionary')
-          -- require('hover.providers.highlight')
-        end,
+      opts = {
+        providers = {
+          'hover.providers.diagnostic',
+          'hover.providers.lsp',
+          'hover.providers.dap',
+          'hover.providers.man',
+          'hover.providers.dictionary',
+          -- Optional, disabled by default:
+          -- 'hover.providers.gh',
+          -- 'hover.providers.gh_user',
+          -- 'hover.providers.jira',
+          -- 'hover.providers.fold_preview',
+          -- 'hover.providers.highlight',
+        },
         preview_opts = {
           border = 'single',
         },
@@ -63,23 +63,33 @@ return {
         preview_window = false,
         title = true,
         mouse_providers = {
-          'LSP',
+          'hover.providers.lsp',
         },
         mouse_delay = 1000,
       }
 
       -- Setup keymaps
-      vim.keymap.set('n', 'K', require('hover').hover, { desc = 'hover.nvim' })
-      vim.keymap.set('n', 'gK', require('hover').hover_select, { desc = 'hover.nvim (select)' })
+      vim.keymap.set('n', 'K', function()
+        require('hover').open()
+      end, { desc = 'hover.nvim (open)' })
+
+      vim.keymap.set('n', 'gK', function()
+        require('hover').enter()
+      end, { desc = 'hover.nvim (enter)' })
+
       vim.keymap.set('n', '<C-p>', function()
-        require('hover').hover_switch 'previous'
+        require('hover').switch 'previous'
       end, { desc = 'hover.nvim (previous source)' })
+
       vim.keymap.set('n', '<C-n>', function()
-        require('hover').hover_switch 'next'
+        require('hover').switch 'next'
       end, { desc = 'hover.nvim (next source)' })
 
       -- Mouse support
-      vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = 'hover.nvim (mouse)' })
+      vim.keymap.set('n', '<MouseMove>', function()
+        require('hover').mouse()
+      end, { desc = 'hover.nvim (mouse)' })
+
       vim.o.mousemoveevent = true
     end,
   },
