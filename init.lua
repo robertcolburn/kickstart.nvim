2259a7e196175f30e83208e374267f9bc8e3353e
robertcolburn <60761375+robertcolburn@users.noreply.github.com>
2025-11-04 19:24:40 -0600

    Fix shiftwidth not being applied

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ vim.g.maplocalleader = ' '
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.shiftwidth = 4
+vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.wrap = false
