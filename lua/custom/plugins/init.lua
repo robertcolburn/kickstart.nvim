298c44dd20b28bcf598622602fcf9b17452d060c
robertcolburn <60761375+robertcolburn@users.noreply.github.com>
2026-03-11 16:57:59 -0500

    Formatting

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -26,9 +26,7 @@ return {
       'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
-    init = function()
-      vim.g.barbar_auto_setup = false
-    end,
+    init = function() vim.g.barbar_auto_setup = false end,
     opts = {
       -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
       -- animation = true,
